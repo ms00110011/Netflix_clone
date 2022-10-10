c39b1ad37fc0d5903317841fbe0bbecbcf69c220
ms00110011 <ms00110011@gmail.com>
2022-10-10 18:57:36 +0530

    Created First Banner of welcome page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import { WelcomePage } from './Components/Welcome Page/WelcomePage';
 
 function App() {
   return (
     <div className="App">
-      <WelcomePage />
+    
+    <WelcomePage />
+
     </div>
   );
 }
--- src/Components/Welcome Page/WelcomePage.jsx
+++ src/Components/Welcome Page/WelcomePage.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-import "./WelcomePage.css"
+import React from "react";
+import "./WelcomePage.css";
+import logo from "../../netflix.svg";
 
 export const WelcomePage = () => {
   return (
-    <div>WelcomePage</div>
-  )
-}
+    <div>
+      <div className="Banner1">
+        <div className="Header">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+
+          <div className="SignIn">
+            <button className='SignIn__button'>Sign In</button>
+          </div>
+        </div>
+
+        <div className="StoryCard">
+            <div className="StoryCard1">Unlimited movies, TV shows and more.</div>
+            <div className="StoryCard2">Watch anywhere. Cancel anytime.</div>
+            <div className="StoryCard3">Ready to watch? Enter your email to create or restart your membership.</div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
