bb91f30d4c21f21c0ce381dbf7ded98ad9da6006
ms00110011 <ms00110011@gmail.com>
2022-10-12 17:25:29 +0530

    Integrated Mui, added Email Input to welcome page

--- src/Components/Welcome Page/WelcomePage.jsx
+++ src/Components/Welcome Page/WelcomePage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./WelcomePage.css";
 import logo from "../../netflix.svg";
+import TextField from "@mui/material/TextField";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 export const WelcomePage = () => {
   return (
@@ -12,16 +14,28 @@ export const WelcomePage = () => {
           </div>
 
           <div className="SignIn">
-            <button className='SignIn__button'>Sign In</button>
+            <button className="SignIn__button">Sign In</button>
           </div>
         </div>
 
         <div className="StoryCard">
-            <div className="StoryCard1">Unlimited movies, TV shows and more.</div>
-            <div className="StoryCard2">Watch anywhere. Cancel anytime.</div>
-            <div className="StoryCard3">Ready to watch? Enter your email to create or restart your membership.</div>
+          <div className="StoryCard1">Unlimited movies, TV shows and more.</div>
+          <div className="StoryCard2">Watch anywhere. Cancel anytime.</div>
+          <div className="StoryCard3">
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </div>
         </div>
 
+        <div className="EmailInput">
+          <TextField
+            sx={{ backgroundColor: "white", width: "35%" }}
+            id="filled-basic"
+            label="Email address"
+            variant="filled"
+          />
+          <button className="Started__Button">Get Started <ArrowForwardIosIcon sx={{width:"0px"}} /> </button>
+        </div>
       </div>
     </div>
   );
