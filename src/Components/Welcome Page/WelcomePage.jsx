262b80b1fef27d99602c7fea6f7d62d1ff526b4f
ms00110011 <ms00110011@gmail.com>
2022-10-13 19:03:48 +0530

    Added banner 2 and video

--- src/Components/Welcome Page/WelcomePage.jsx
+++ src/Components/Welcome Page/WelcomePage.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./WelcomePage.css";
 import logo from "../../netflix.svg";
 import TextField from "@mui/material/TextField";
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ReactPlayer from "react-player";
 
 export const WelcomePage = () => {
   return (
@@ -34,7 +35,32 @@ export const WelcomePage = () => {
             label="Email address"
             variant="filled"
           />
-          <button className="Started__Button">Get Started <ArrowForwardIosIcon sx={{width:"0px"}} /> </button>
+          <button className="Started__Button">
+            Get Started <ArrowForwardIosIcon sx={{ width: "0px" }} />{" "}
+          </button>
+        </div>
+      </div>
+
+      <div className="Banner2">
+        <div className="Banner_text">
+          <h1>Enjoy on your TV.</h1>
+          <p>
+            Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray
+            players and more.
+          </p>
+        </div>
+        <div className="Banner_media">
+        <video autoPlay playsInline loop>
+            <source
+              src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v"
+              type="video/mp4"
+            />
+          </video>
+          <img
+            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
+            alt=""
+          />
+
         </div>
       </div>
     </div>
